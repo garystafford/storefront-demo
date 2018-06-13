a401fa6e060a2f8e670f96bbe73d909d5ab40bf6
Gary A. Stafford <garystafford@rochester.rr.com>
2018-06-13 16:18:54 -0400

    git helper script

---
+++ git_helper.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# usage: sh ./git_helper.sh "Refactoring code"
+
+args=("$@")
+
+root="/Users/garystafford/Documents/projects"
+repos=(
+  storefront-demo-accounts
+  storefront-demo-orders
+  storefront-demo-fulfillment
+  storefront-eureka-server
+  storefront-zuul-proxy
+)
+if [ -n "${args[0]}" ]; then
+    comment=${args[0]}
+else
+    comment="Changes to project..."
+fi
+
+echo ${comment}
+
+for repo in "${repos[@]}"
+do
+  cd ${root}/${repo} && \
+  git add -A && \
+  git commit -m "${comment}" && \
+  git push && \
+  git status
+done
+
+cd ../
