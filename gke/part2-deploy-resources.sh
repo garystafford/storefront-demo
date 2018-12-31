e9724bda7a56b178a172f02a8a09be32458f4f6c
Gary A. Stafford <garystafford@rochester.rr.com>
2018-12-31 00:11:46 -0500

    External Mesh for MongoDB working

--- gke/part2-deploy-resources.sh
+++ gke/part2-deploy-resources.sh
@@ -9,6 +9,8 @@ export REGION="us-central1"
 export ZONE="us-central1-a"
 
 kubectl apply -f ./resources/other/istio-gateway-multi-ns.yaml
+kubectl apply -f ./resources/other/mongodb-atlas-external-mesh.yaml
+kubectl apply -f ./resources/other/confluent-cloud-external-mesh.yaml
 
 kubectl delete configmap confluent-cloud-kafka -n $NAMESPACE
 kubectl delete secret mongodb-atlas -n $NAMESPACE
@@ -32,9 +34,9 @@ kubectl get secret confluent-cloud-kafka -n $NAMESPACE --export -o yaml \
 kubectl get secret confluent-cloud-kafka -n $NAMESPACE --export -o yaml \
   | kubectl apply -n staging -f -
 
-kubectl delete deployment accounts -n $NAMESPACE
-kubectl delete deployment fulfillment -n $NAMESPACE
-kubectl delete deployment orders -n $NAMESPACE
+# kubectl delete deployment accounts -n $NAMESPACE
+# kubectl delete deployment fulfillment -n $NAMESPACE
+# kubectl delete deployment orders -n $NAMESPACE
 
 kubectl apply -n $NAMESPACE -f ./resources/services/accounts.yaml
 kubectl apply -n $NAMESPACE -f ./resources/services/fulfillment.yaml
