2b1267eaeab5881d8b6a33f877cffc9ea8041e20
Gary A. Stafford <garystafford@rochester.rr.com>
2018-12-30 22:13:14 -0500

    Refactor scripts

--- gke/part2-deploy-resources.sh
+++ gke/part2-deploy-resources.sh
@@ -11,8 +11,8 @@ export ZONE="us-central1-a"
 kubectl apply ./resources/other/istio-gateway.yaml
 
 kubectl apply -n $NAMESPACE -f ./resources/config/confluent-cloud-kafka-configmap.yaml
-kubectl apply -n $NAMESPACE -f ../../storefront-secrets/mongodb-atlas-secret.yaml
-kubectl apply -n $NAMESPACE -f ../../storefront-secrets/confluent-cloud-kafka-secret.yaml
+kubectl apply -n $NAMESPACE -f ./resources/config/mongodb-atlas-secret.yaml
+kubectl apply -n $NAMESPACE -f ./resources/config/confluent-cloud-kafka-secret.yaml
 
 kubectl apply -n $NAMESPACE -f ./resources/services/accounts.yaml
 kubectl apply -n $NAMESPACE -f ./resources/services/fulfillment.yaml
