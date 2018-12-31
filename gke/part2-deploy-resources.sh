4f79283c5f70ca829949bab276cc16aad6f6f8a0
Gary A. Stafford <garystafford@rochester.rr.com>
2018-12-31 12:50:17 -0500

    Refactor python script

--- gke/part2-deploy-resources.sh
+++ gke/part2-deploy-resources.sh
@@ -12,9 +12,9 @@ kubectl apply -f ./resources/other/istio-gateway-multi-ns.yaml
 kubectl apply -f ./resources/other/mongodb-atlas-external-mesh.yaml
 kubectl apply -f ./resources/other/confluent-cloud-external-mesh.yaml
 
-kubectl delete configmap confluent-cloud-kafka -n $NAMESPACE
-kubectl delete secret mongodb-atlas -n $NAMESPACE
-kubectl delete secret confluent-cloud-kafka -n $NAMESPACE
+# kubectl delete configmap confluent-cloud-kafka -n $NAMESPACE
+# kubectl delete secret mongodb-atlas -n $NAMESPACE
+# kubectl delete secret confluent-cloud-kafka -n $NAMESPACE
 
 kubectl apply -n $NAMESPACE -f ./resources/config/confluent-cloud-kafka-configmap.yaml
 kubectl get configmap confluent-cloud-kafka -n $NAMESPACE --export -o yaml \
@@ -34,9 +34,9 @@ kubectl get secret confluent-cloud-kafka -n $NAMESPACE --export -o yaml \
 kubectl get secret confluent-cloud-kafka -n $NAMESPACE --export -o yaml \
   | kubectl apply -n staging -f -
 
-kubectl delete deployment accounts -n $NAMESPACE
-kubectl delete deployment fulfillment -n $NAMESPACE
-kubectl delete deployment orders -n $NAMESPACE
+# kubectl delete deployment accounts -n $NAMESPACE
+# kubectl delete deployment fulfillment -n $NAMESPACE
+# kubectl delete deployment orders -n $NAMESPACE
 
 kubectl apply -n $NAMESPACE -f ./resources/services/accounts.yaml
 kubectl apply -n $NAMESPACE -f ./resources/services/fulfillment.yaml
