8af3dcaa5e163c69fc509579b27cdd69830e332b
Gary A. Stafford <garystafford@rochester.rr.com>
2019-01-13 09:47:37 -0500

    Refactor set-up scripts

--- gke/part2_deploy_resources.sh
+++ gke/part2_deploy_resources.sh
@@ -14,7 +14,7 @@ kubectl apply -f ./resources/other/istio-gateway.yaml
 kubectl apply -n $NAMESPACE -f ./resources/other/mongodb-atlas-external-mesh.yaml
 kubectl apply -n $NAMESPACE -f ./resources/other/confluent-cloud-external-mesh.yaml
 
-kubectl apply -f ./resources/config/confluent-cloud-kafka-configmap.yaml
+kubectl apply -n $NAMESPACE -f ./resources/config/confluent-cloud-kafka-configmap.yaml
 kubectl apply -f ./resources/config/mongodb-atlas-secret.yaml
 kubectl apply -f ./resources/config/confluent-cloud-kafka-secret.yaml
 
