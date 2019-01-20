f567f3a8171f5bb6a39d37ad17ce1b59bf1fe7eb
Gary A. Stafford <garystafford@rochester.rr.com>
2019-01-19 19:36:47 -0500

    Update resources

--- gke/part1_create_environment.sh
+++ gke/part1_create_environment.sh
@@ -12,27 +12,27 @@ readonly ZONE='us-central1-a'
 # Create GKE cluster (time in foreground)
 time \
   gcloud beta container \
-    --project $PROJECT clusters create $CLUSTER \
-    --zone $ZONE \
-    --username "admin" \
-    --cluster-version "1.11.5-gke.5" \
-    --machine-type "n1-standard-1" \
-    --image-type "COS" \
-    --disk-type "pd-standard" \
-    --disk-size "100" \
-    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-    --num-nodes "2" \
-    --enable-stackdriver-kubernetes \
-    --enable-ip-alias \
-    --network "projects/$PROJECT/global/networks/default" \
-    --subnetwork "projects/$PROJECT/regions/$REGION/subnetworks/default" \
-    --default-max-pods-per-node "110" \
-    --addons HorizontalPodAutoscaling,HttpLoadBalancing,Istio \
-    --istio-config auth=MTLS_PERMISSIVE \
-    --issue-client-certificate \
-    --metadata disable-legacy-endpoints=true \
-    --enable-autoupgrade \
-    --enable-autorepair
+  --project $PROJECT clusters create $CLUSTER \
+  --zone $ZONE \
+  --username "admin" \
+  --cluster-version "1.11.5-gke.5" \
+  --machine-type "n1-standard-2" \
+  --image-type "COS" \
+  --disk-type "pd-standard" \
+  --disk-size "100" \
+  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+  --num-nodes "2" \
+  --enable-stackdriver-kubernetes \
+  --enable-ip-alias \
+  --network "projects/$PROJECT/global/networks/default" \
+  --subnetwork "projects/$PROJECT/regions/$REGION/subnetworks/default" \
+  --default-max-pods-per-node "110" \
+  --addons HorizontalPodAutoscaling,HttpLoadBalancing,Istio \
+  --istio-config auth=MTLS_PERMISSIVE \
+  --issue-client-certificate \
+  --metadata disable-legacy-endpoints=true \
+  --enable-autoupgrade \
+  --enable-autorepair
 
 # Get cluster creds
 gcloud container clusters get-credentials $CLUSTER \
