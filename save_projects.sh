46ce89e3d1398f89ca2bf2ba3e216d7c5f738f44
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-10 15:03:18 -0400

    Updating for 2021.05 release

--- save_projects.sh
+++ save_projects.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-# Usage (from root of project): sh ./Docker/build_images.sh
+# Usage (from root of project): sh ./Docker/save_projects.sh
+
+COMMIT_MESSAGE="Updating for 2021.05 release"
 
 cd /Users/garystafford/Documents/projects/storefront-kafka-docker || exit
 pwd
-git add -A && git commit -m"Updating for 2021.05 release"
+git add -A && git commit -m"${COMMIT_MESSAGE}" && git push
 
 cd ../storefront-demo-accounts || exit
 pwd
-git add -A && git commit -m"Updating for 2021.05 release"
+git add -A && git commit -m"${COMMIT_MESSAGE}" && git push
 
 cd ../storefront-demo-orders || exit
 pwd
-git add -A && git commit -m"Updating for 2021.05 release"
+git add -A && git commit -m"${COMMIT_MESSAGE}" && git push
 
 cd ../storefront-demo-fulfillment || exit
 pwd
-git add -A && git commit -m"Updating for 2021.05 release"
-
+git add -A && git commit -m"${COMMIT_MESSAGE}" && git push
 
 #docker pull garystafford/storefront-fulfillment:3.0.0 \
 #&& docker pull garystafford/storefront-accounts:3.0.0 \
