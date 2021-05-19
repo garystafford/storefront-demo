8d18f91b8243aedbc6d2d29411fa640bf7545f6c
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-19 07:12:33 -0400

    Clean up old files

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -3,6 +3,7 @@
 # Usage (from root of project): sh ./Docker/build_images.sh
 
 IMAGE_TAG=3.0.0
+IMAGE_TAG_MINIKUBE=3.0.0-minikube
 
 cd /Users/garystafford/Documents/projects/storefront-demo-accounts || exit
 pwd
@@ -10,32 +11,32 @@ docker build -f Docker/Dockerfile_base --no-cache -t garystafford/storefront-bas
 docker push garystafford/storefront-base:$IMAGE_TAG
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:$IMAGE_TAG .
-docker push garystafford/storefront-accounts:$IMAGE_TAG
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:$IMAGE_TAG_MINIKUBE .
+docker push garystafford/storefront-accounts:$IMAGE_TAG_MINIKUBE
 
 cd ../storefront-demo-orders || exit
 pwd
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:$IMAGE_TAG .
-docker push garystafford/storefront-orders:$IMAGE_TAG
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:$IMAGE_TAG_MINIKUBE .
+docker push garystafford/storefront-orders:$IMAGE_TAG_MINIKUBE
 
 cd ../storefront-demo-fulfillment || exit
 pwd
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-fulfillment:$IMAGE_TAG .
-docker push garystafford/storefront-fulfillment:$IMAGE_TAG
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-fulfillment:$IMAGE_TAG_MINIKUBE .
+docker push garystafford/storefront-fulfillment:$IMAGE_TAG_MINIKUBE
 
-cd ../storefront-eureka-server || exit
-pwd
-./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-eureka:$IMAGE_TAG .
-docker push garystafford/storefront-eureka:$IMAGE_TAG
-
-cd ../storefront-zuul-proxy || exit
-pwd
-./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-zuul:$IMAGE_TAG .
-docker push garystafford/storefront-zuul:$IMAGE_TAG
+#cd ../storefront-eureka-server || exit
+#pwd
+#./gradlew clean build
+#docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-eureka:$IMAGE_TAG .
+#docker push garystafford/storefront-eureka:$IMAGE_TAG
+#
+#cd ../storefront-zuul-proxy || exit
+#pwd
+#./gradlew clean build
+#docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-zuul:$IMAGE_TAG .
+#docker push garystafford/storefront-zuul:$IMAGE_TAG
 
 
 #docker pull garystafford/storefront-fulfillment:$IMAGE_TAG \
