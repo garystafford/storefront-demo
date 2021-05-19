2f37630d0c746bd5af7a55fbd73980efd6e616eb
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-19 09:01:57 -0400

    Update script

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -8,40 +8,40 @@ IMAGE_TAG_MINIKUBE=3.0.0-minikube
 cd /Users/garystafford/Documents/projects/storefront-demo-accounts || exit
 pwd
 docker build -f Docker/Dockerfile_base --no-cache -t garystafford/storefront-base:$IMAGE_TAG .
-docker push garystafford/storefront-base:$IMAGE_TAG
+docker push docker.io/garystafford/storefront-base:$IMAGE_TAG
 
 ./gradlew clean build
 docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:$IMAGE_TAG_MINIKUBE .
-docker push garystafford/storefront-accounts:$IMAGE_TAG_MINIKUBE
+docker push docker.io/garystafford/storefront-accounts:$IMAGE_TAG_MINIKUBE
 
 cd ../storefront-demo-orders || exit
 pwd
 ./gradlew clean build
 docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-orders:$IMAGE_TAG_MINIKUBE .
-docker push garystafford/storefront-orders:$IMAGE_TAG_MINIKUBE
+docker push docker.io/garystafford/storefront-orders:$IMAGE_TAG_MINIKUBE
 
 cd ../storefront-demo-fulfillment || exit
 pwd
 ./gradlew clean build
 docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-fulfillment:$IMAGE_TAG_MINIKUBE .
-docker push garystafford/storefront-fulfillment:$IMAGE_TAG_MINIKUBE
+docker push docker.io/garystafford/storefront-fulfillment:$IMAGE_TAG_MINIKUBE
 
 #cd ../storefront-eureka-server || exit
 #pwd
 #./gradlew clean build
 #docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-eureka:$IMAGE_TAG .
-#docker push garystafford/storefront-eureka:$IMAGE_TAG
+#docker push docker.io/garystafford/storefront-eureka:$IMAGE_TAG
 #
 #cd ../storefront-zuul-proxy || exit
 #pwd
 #./gradlew clean build
 #docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-zuul:$IMAGE_TAG .
-#docker push garystafford/storefront-zuul:$IMAGE_TAG
+#docker push docker.io/garystafford/storefront-zuul:$IMAGE_TAG
 
 
-#docker pull garystafford/storefront-fulfillment:$IMAGE_TAG \
-#&& docker pull garystafford/storefront-accounts:$IMAGE_TAG \
-#&& docker pull garystafford/storefront-orders:$IMAGE_TAG \
+#docker pull docker.io/garystafford/storefront-fulfillment:$IMAGE_TAG \
+#&& docker pull docker.io/garystafford/storefront-accounts:$IMAGE_TAG \
+#&& docker pull docker.io/garystafford/storefront-orders:$IMAGE_TAG \
 #&& docker container ls
 
 #yes | docker system prune
